7b22e1dbec7d7cc72ee5939858ad8b4a49605688
ZEugenii <zinich911@gmail.com>
2022-11-21 15:15:03 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,23 +8,23 @@ function getRandomHexColor() {
 
 export const Statistics = ({ title, stats }) => {
 
-    return (
-        <section className={css.statistics}>
-     {title && <h2 className={css.title}>{title}</h2>}
-        
-            <ul className={css.stat_list}>
-                
-    {stats.map(({id, label, percentage}) => {
-       return (<li key={id}
-            className={css.item}
-            style={{
-              backgroundColor: getRandomHexColor(),
-            }} >
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.stat_list}>
+        {stats.map(({id, label, percentage}) => {
+          return (
+      <li
+          key={id}
+          className={css.item}
+          style={{
+          backgroundColor: getRandomHexColor(),
+          }} >
         <span className={css.label}>{label}</span>
-      <span className={css.percentage}>{percentage}%</span>
+        <span className={css.percentage}>{percentage}%</span>
     </li>)
-})}
-  </ul>
+      })}
+    </ul>
 </section>
     )
 }
